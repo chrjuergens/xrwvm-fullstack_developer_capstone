06cea0f98fd8ca869418bb8b122e9ef10462fbaa
chrjuergens <cjuergens@mailbox.org>
2024-11-19 07:38:52 -0500

    Added 'npm install' and 'npm run build' to setup process

--- setup.sh
+++ setup.sh
@@ -2,11 +2,16 @@
 
 set -euox pipefail
 
+ROOT_DIR=$(pwd)
 cd server/
 pip install virtualenv
 virtualenv djangoenv
 source djangoenv/bin/activate
 python3 -m pip install -U -r requirements.txt
+cd $ROOT_DIR/server/frontend
+npm install
+npm run build
+cd $ROOT_DIR/server
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver
