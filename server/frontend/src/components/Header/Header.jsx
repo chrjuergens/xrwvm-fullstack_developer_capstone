e1be5f02420c5ec19515ff5de3b94d610e38e7f5
chrjuergens <cjuergens@mailbox.org>
2024-11-20 03:26:38 -0500

    Improved readability in html/jsx files

--- server/frontend/src/components/Header/Header.jsx
+++ server/frontend/src/components/Header/Header.jsx
@@ -4,38 +4,36 @@ import "../assets/bootstrap.min.css";
 
 const Header = () => {
     const logout = async (e) => {
-    e.preventDefault();
-    let logout_url = window.location.origin+"/djangoapp/logout";
-    const res = await fetch(logout_url, {
-      method: "GET",
-    });
+        e.preventDefault();
+        let logout_url = window.location.origin+"/djangoapp/logout";
+        const res = await fetch(logout_url, {method: "GET",});
   
-    const json = await res.json();
-    if (json) {
-      let username = sessionStorage.getItem('username');
-      sessionStorage.removeItem('username');
-      window.location.href = window.location.origin;
-      window.location.reload();
-      alert("Logging out "+username+"...")
+        const json = await res.json();
+        if (json) {
+            let username = sessionStorage.getItem('username');
+            sessionStorage.removeItem('username');
+            window.location.href = window.location.origin;
+            window.location.reload();
+            alert("Logging out "+username+"...")
+        } else {
+            alert("The user could not be logged out.")
+        }
+    };
+
+    //The default home page items are the login details panel
+    let home_page_items =  <div></div>
+
+    //Gets the username in the current session
+    let curr_user = sessionStorage.getItem('username')
+
+    //If the user is logged in, show the username and logout option on home page
+    if ( curr_user !== null &&  curr_user !== "") {
+        home_page_items = <div className="input_panel">
+        <text className='username'>{sessionStorage.getItem("username")}</text>
+        <a className="nav_item" href="/djangoapp/logout" onClick={logout}>Logout</a>
+        </div>
     }
-    else {
-      alert("The user could not be logged out.")
-    }
-  };
     
-//The default home page items are the login details panel
-let home_page_items =  <div></div>
-
-//Gets the username in the current session
-let curr_user = sessionStorage.getItem('username')
-
-//If the user is logged in, show the username and logout option on home page
-if ( curr_user !== null &&  curr_user !== "") {
-    home_page_items = <div className="input_panel">
-      <text className='username'>{sessionStorage.getItem("username")}</text>
-    <a className="nav_item" href="/djangoapp/logout" onClick={logout}>Logout</a>
-  </div>
-}
     return (
         <div>
           <nav class="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"darkturquoise",height:"1in"}}>
