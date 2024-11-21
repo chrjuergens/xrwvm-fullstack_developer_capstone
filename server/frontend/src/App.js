4804d6194c15d75886c6be537024461c737c7452
chrjuergens <cjuergens@mailbox.org>
2024-11-21 08:51:27 -0500

    Added Dealer component and route to frontend

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,7 @@ import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 import Dealers from "./components/Dealers/Dealers";
+import Dealer from "./components/Dealers/Dealer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
       <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
     </Routes>
   );
 }
