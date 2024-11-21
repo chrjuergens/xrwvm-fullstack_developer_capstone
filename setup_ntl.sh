a67a94b2bd640367b1a6bbb8c1f4b131291f395e
chrjuergens <cjuergens@mailbox.org>
2024-11-21 06:51:34 -0500

    Setup script for sentiment analyzer updated

--- setup_ntl.sh
+++ setup_ntl.sh
@@ -2,6 +2,10 @@
 
 set -euox pipefail
 
+ROOT_DIR=$(pwd)
+
+cd $ROOT_DIR/server/djangoapp/microservices/
+
 docker build . -t us.icr.io/${SN_ICR_NAMESPACE}/senti_analyzer
 docker push us.icr.io/${SN_ICR_NAMESPACE}/senti_analyzer
 ibmcloud ce application create --name sentianalyzer --image us.icr.io/${SN_ICR_NAMESPACE}/senti_analyzer --registry-secret icr-secret --port 5000
