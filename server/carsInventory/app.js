1077a148b922db2386f335a9bed8c91c21d0349d
chrjuergens <cjuergens@mailbox.org>
2024-11-29 06:50:51 -0500

    Added express files: app.js (express server), inventory.js (schema), cars.json (data)

--- server/carsInventory/app.js
+++ server/carsInventory/app.js
@@ -1,13 +1,131 @@
 const mongoose = require('mongoose');
-const mongodb = require('mongodb');
 const fs = require('fs');
+const express = require('express');
+const cors = require('cors');
 
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
-};
+const app = express();
+const port = 3050;
 
-const get_car_records = await fs.readFile('data/car_records.json', (data, err) => {
-    return JSON.parse(data);
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+/*
+const carsData = Promise.resolve()
+    .then(() => fs.readFileSync('./data/car_records.json'))
+    .then(res => JSON.parse(res))
+    .catch(err => console.error('Error in loading car records:', err));
+*/
+
+const carsData = JSON.parse(fs.readFileSync('./data/car_records.json', 'utf8'));
+
+mongoose.connect('mongodb://mongo_db:27017/', { dbName: 'dealershipsDB' })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+const Cars = require('./inventory.js');
+
+
+Cars.deleteMany({})
+    .then(() => Cars.insertMany(carsData.cars))
+    .catch(err => console.error('Error preparing cars data:', err));
+
+Cars.findOne({})
+    .then(res => console.log(res))
+    .catch(err => console.error('Error finding a single car:', err));
+
+
+// API endpoints //
+
+app.route('/', async (req, res) => {
+    res.send('Welcome to the Mongoose API');
 });
 
-mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
+app.route('/cars/:id', async (req, res) => {
+    try {
+        const documents = await Cars.find({dealer_id: req.params.id});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({error: 'Error fetching cars by id'});
+    }
+});
+
+app.route('/carsbymake/:id/:make', async (req, res) => {
+    try {
+        const documents = await Cars.find({
+            dealer_id: req.params.id,
+            make: req.params.make
+        });
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({error: 'Error fetching cars by id and make'});
+    }
+});
+
+app.get('/carsbymodel/:id/:model', async (req, res) => {
+    try {
+        const documents = await Cars.find({
+            dealer_id: req.params.id,
+            model: req.params.model
+        });
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers by ID' });
+    }
+});
+
+app.get('/carsbymaxmileage/:id/:mileage', async (req, res) => {
+    try {
+        let mileage = parseInt(req.params.mileage)
+        let condition = {}
+        if(mileage === 50000) {
+            condition = { $lte : mileage}
+        } else if (mileage === 100000){
+            condition = { $lte : mileage, $gt : 50000}
+        } else if (mileage === 150000){
+            condition = { $lte : mileage, $gt : 100000}
+        } else if (mileage === 200000){
+            condition = { $lte : mileage, $gt : 150000}
+        } else {
+            condition = { $gt : 200000}
+        }
+        const documents = await Cars.find({ dealer_id: req.params.id, mileage : condition });
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers by ID' });
+    }
+  });
+
+app.get('/carsbyprice/:id/:price', async (req, res) => {
+    try {
+        let price = parseInt(req.params.price)
+        let condition = {}
+        if(price === 20000) {
+            condition = { $lte : price}
+        } else if (price=== 40000){
+            console.log("\n \n \n "+ price)  
+            condition = { $lte : price, $gt : 20000}
+        } else if (price === 60000){
+            condition = { $lte : price, $gt : 40000}
+        } else if (price === 80000){
+            condition = { $lte : price, $gt : 60000}
+        } else {
+            condition = { $gt : 80000}
+        }
+        const documents = await Cars.find({ dealer_id: req.params.id, price : condition });
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers by ID' });
+    }
+});
+
+app.get('/carsbyyear/:id/:year', async (req, res) => {
+    try {
+        const documents = await Cars.find({ dealer_id: req.params.id, year : { $gte :req.params.year }});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers by ID' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
